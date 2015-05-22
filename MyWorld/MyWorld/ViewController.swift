55a3c86aae81a69fc5f34f79e17aff9fe62e6d0e
j-pk <parker.kirby@gmail.com>
2015-05-22 17:33:44 -0400

    Update to apps and MyWorld upload

--- MyWorld/MyWorld/ViewController.swift
+++ MyWorld/MyWorld/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 import MapKit
+
+class myAnnotation : MKPointAnnotation {
+    
+    var venueIndex: Int!
+    
+}
+
 //whenever working with a delegate we need to set the delegate
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
     @IBOutlet weak var myMapView: MKMapView!
     
     var lManager = CLLocationManager()
+    var allVenues: [[String:AnyObject]] = []
+    
+    var venuesAddress: String?
+    
+    let API_URL = "https://api.foursquare.com/v2/"
+    let CLIENT_ID = "MZB35JAM3XG14YZXYTSDNP2F1N131MZA1LFMIRD1T23JQPL4"
+    let CLIENT_SECRET = "XHRTWYZZWXVIJHVXFO0H5DVSKWODQP3DUEVA5LOQEGVWSVXW"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +38,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         myMapView.showsUserLocation = true
         myMapView.userTrackingMode = .Follow
+        myMapView.delegate = self
         
         lManager.startUpdatingLocation()
+        
+        navigationController?.navigationBar.barTintColor = UIColor(red:1, green:0.76, blue:0.28, alpha:1)
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         
     }
     
@@ -37,8 +57,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             requestVenuesWithLocation(location, completion: { (venues) -> () in
                 
                 println (venues)
+                self.allVenues = venues as! [[String:AnyObject]]
                 
-                for venue in venues as! [[String:AnyObject]] {
+                for (index, venue) in enumerate(venues as! [[String:AnyObject]]) {
                     
                     if let locationInfo = venue["location"] as? [String:AnyObject] {
                         
@@ -46,10 +67,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                             
                             let coordinate = CLLocationCoordinate2DMake(lat, lng)
                             
-                            let annotation = MKPointAnnotation()
+                            let annotation = myAnnotation()
                             
                             annotation.title = venue["name"] as? String
-                            
+                            annotation.venueIndex = index
                             annotation.coordinate = coordinate
                             
                             self.myMapView.addAnnotation(annotation)
@@ -71,11 +92,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func requestVenuesWithLocation(location: CLLocation, completion: (venues: [AnyObject]) -> ()) {
-        
-        let API_URL = "https://api.foursquare.com/v2/"
-        let CLIENT_ID = "MZB35JAM3XG14YZXYTSDNP2F1N131MZA1LFMIRD1T23JQPL4"
-        let CLIENT_SECRET = "XHRTWYZZWXVIJHVXFO0H5DVSKWODQP3DUEVA5LOQEGVWSVXW"
-        
+
         //specific endpoint to find around user location
         let endpoint = API_URL + "venues/search?client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&ll=\(location.coordinate.latitude),\(location.coordinate.longitude)&v=20150101"
         
@@ -107,8 +124,108 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 
             })
             
+            
         }
         
+    }
+    
+
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        if annotation is MKUserLocation { return nil }
+        
+        var annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "annotation")
+        
+        //customizing the pin color and allowing the title bubble to show
+        annotationView.pinColor = MKPinAnnotationColor.Green
+        annotationView.canShowCallout = true
+        
+        //allows the I button to be display when a pin is pressed
+        var button = UIButton.buttonWithType(UIButtonType.DetailDisclosure) as! UIButton
+        annotationView.rightCalloutAccessoryView = button
+        
+        button.tag = (annotation as! myAnnotation).venueIndex
+        
+        //target the showMoreInfo func self has showMoreInfo, action is init the showMoreInfo func
+        //action if leveraging a button with a sender it must have a : if you have a parameter
+        button.addTarget(self, action: "showMoreInfo:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        return annotationView
+        
+    }
+    
+    func showMoreInfo(sender: UIButton) {
+        
+        var detailVC = UIViewController()
+        
+        var venue = allVenues[sender.tag]
+        
+        println(detailVC)
+        
+        detailVC.view.backgroundColor = UIColor.whiteColor()
+        
+        detailVC.navigationItem.title = venue["name"] as? String
+        
+        if let category = venue["categories"] as? [[String:AnyObject]] {
+            
+            let insideCategoryArray = category[0]
+            let venueCat = insideCategoryArray["name"] as! String
+//            let prefix = 
+            
+            //little circle on the left
+            var firstCircle = UIView(frame: CGRectMake(30, 100, 40, 40))
+            firstCircle.layer.cornerRadius = firstCircle.frame.size.width / 2
+            firstCircle.backgroundColor = UIColor(red:1, green:0.76, blue:0.28, alpha:1)
+            
+//            let circleURL = NSURL(string: (insideCategoryArray["prefix" + "32" + ".png"] as? String)!)
+//            let imageData = NSData(contentsOfURL: circleURL!)
+//            let circleUIImage = UIImage(data: imageData!)
+            
+//            println(imageData)
+            
+            //Divider
+            var boxDivider = UIView(frame: CGRectMake(90, 100, 5, 40))
+            boxDivider.backgroundColor = UIColor(red:0.69, green:0.69, blue:0.69, alpha:1)
+            
+            
+            //Venue Category Name
+            var boxCat = UILabel(frame: CGRectMake(110, 90, 400, 40))
+            var boxCatName = UILabel(frame: CGRectMake(110, 110, 400, 40))
+            
+            boxCat.text = "Category"
+            boxCatName.text = venueCat
+            boxCatName.font = UIFont(name: "HelveticaNeue-Light", size: 22)
+            boxCat.font = UIFont(name: "HelveticaNeue-Light", size: 14)
+            
+            detailVC.view.addSubview(boxCat)
+            detailVC.view.addSubview(boxCatName)
+            detailVC.view.addSubview(boxDivider)
+            detailVC.view.addSubview(firstCircle)
+//            detailVC.view.addSubview(circleUIImage)
+            
+        }
+        
+        if let location = venue["location"] as? [String:AnyObject] {
+            
+            var address = location["address"] as? String
+            var city = location["city"] as? String
+            var state = location["state"] as? String
+            
+            //Address Name 
+            var boxAddress = UITextField(frame: CGRectMake(110, 180, 300, 300))
+            var boxAddressDivider = UIView(frame: CGRectMake(90, 180, 5, 180))
+            boxAddressDivider.backgroundColor = UIColor(red:0.69, green:0.69, blue:0.69, alpha:1)
+            
+            detailVC.view.addSubview(boxAddressDivider)
+            detailVC.view.addSubview(boxAddress)
+
+            boxAddress.text = address
+            
+        }
+        
+
+        navigationController?.pushViewController(detailVC, animated: true)
+
     }
     
 }
--- SimpleButtons/SimpleButtons/ViewController.swift
+++ SimpleButtons/SimpleButtons/ViewController.swift
@@ -11,72 +11,68 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var printValue: UILabel!
-    var firstNumber = 0
-    var secondNumber = 0
     var value: Double = 0
-    var currentOperator = ""
+    var firstNumber: Int?
+    var secondNumber: Int?
+    var currentOperator = " "
     var percentNumber: Double = 0.01
+    
+    var userIsTypingSomething: Bool = false
     
     //This clears the label, AC Button
     @IBAction func clearButton(sender: UIButton) {
 
           printValue.text = "\(value)"
     }
-    @IBAction func decimalPressed(sender: UIButton) {
-        
-        printValue.text = "\(firstNumber)" + "."
-        
-    }
+
     
     //This is registering the value of the first and second button pressed
     @IBAction func pressedButton1(sender: UIButton) {
         //sender is an arguement, UIButton is an object
         
-        let number = sender.currentTitle!.toInt()!
+        let firstNumber = sender.currentTitle
         
-        if currentOperator.isEmpty {
+        if userIsTypingSomething {
             
-            firstNumber = number
-            println("\(firstNumber)")
-            printValue.text = "\(firstNumber)"
+            printValue.text = printValue.text! + firstNumber!
             
         } else {
             
-            secondNumber = number
-            println("\(secondNumber)")
-            printValue.text = "\(secondNumber)"
+            printValue.text = firstNumber
+            userIsTypingSomething = true
+         
         }
         
     }
     
-    //This is my partially working value changer for int
-    @IBAction func plusMinus(sender: UIButton) {
-        
-        if firstNumber > 0
-        {
-            firstNumber = firstNumber * -1
-            printValue.text = "\(firstNumber)"
-            println(firstNumber)
-        }
-        
-        if firstNumber < 0
-        {
-            firstNumber = firstNumber * 1
-            printValue.text = "\(firstNumber)"
-            println(firstNumber)
-        }
-        
-    }
+//    //This is my partially working value changer for int
+//    @IBAction func plusMinus(sender: UIButton) {
+//        
+//        if firstNumber > 0
+//        {
+//            firstNumber = firstNumber * -1
+//            printValue.text = "\(firstNumber)"
+//            println(firstNumber)
+//        }
+//        
+//        if firstNumber < 0
+//        {
+//            firstNumber = firstNumber * 1
+//            printValue.text = "\(firstNumber)"
+//            println(firstNumber)
+//        }
+//        
+//    }
     
-    //This is my partially working percent or mutliplying a value to 0.01
-    //Let is a static variable
-    @IBAction func percentButton(sender: UIButton) {
-        
-        let value = Double(firstNumber) * percentNumber
-        printValue.text = "\(value)"
-        
-        
-    }
+//    //This is my partially working percent or mutliplying a value to 0.01
+//    //Let is a static variable
+//    @IBAction func percentButton(sender: UIButton) {
+//        
+//        let value = Double(firstNumber) * percentNumber
+//        printValue.text = "\(value)"
+//        
+//        
+//    }
     
     //This is a switch for operator functionality - THIS IS A CONDITIONAL
     @IBAction func operatorPressed(sender: UIButton) {
