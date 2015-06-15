de89a39ad857394386925f6450162712802c90a4
j-pk <parker.kirby@gmail.com>
2015-06-15 13:46:20 -0400

    Uploading messaging app

---
+++ Secrets/Secrets/FriendsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsTableViewController.h
+//  Secrets
+//
+//  Created by jpk on 6/15/15.
+//  Copyright (c) 2015 Parker Kirby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsTableViewController : UITableViewController
+
+@end
---
+++ Secrets/Secrets/SearchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableViewController.h
+//  Secrets
+//
+//  Created by jpk on 6/15/15.
+//  Copyright (c) 2015 Parker Kirby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableViewController : UITableViewController
+
+@end
